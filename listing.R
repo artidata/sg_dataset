f26650931a0b80ce84bfd63d44fbd5806a6a84fe
Imaduddin Haetami <bizih@nus.edu.sg>
2018-09-17 13:10:57 +0800

    using dataset_id as query parameter

--- listing.R
+++ listing.R
@@ -3,26 +3,35 @@ library(curl)
 library(stringr)
 library(data.table)
 
+dir.create(str_c("processed_data/",today()))
+dirOutput <- str_c("processed_data/",today(),"/")
 #download and readlisting dataset
-curl_download("https://data.gov.sg/dataset/dba9594b-fb5c-41c5-bb7c-92860ee31aeb/download/",
-              "processed_data/dataset-listing.zip")
-unzip("processed_data/dataset-listing.zip",exdir ="processed_data/dataset-listing")
-dtListing <- fread("processed_data/dataset-listing/data-gov-sg-dataset-listing.csv")
 
-#setup before querying CKAN API
-query <- "https://data.gov.sg/api/action/datastore_search?"
-parameter <- "resource_id="
-dirOutput <- "processed_data/"
+curl_download("https://data.gov.sg/dataset/dba9594b-fb5c-41c5-bb7c-92860ee31aeb/download/",
+              str_c(dirOutput,"dataset-listing.zip"))
+unzip(str_c(dirOutput,"dataset-listing.zip"),
+      exdir = str_c(dirOutput,"dataset-listing"))
+dtListing <- fread(str_c(dirOutput,
+                         "dataset-listing/data-gov-sg-dataset-listing.csv"))
 
 #navigate and select dataset of interest
 dtListing
 
-#query for downloading dataset
-index <- 5
+#setup before querying CKAN API
+query <- "https://data.gov.sg/dataset/"
+index <- 1140 #Master Plan 2014 Planning Area Boundary (Web) (SHP)
 dtListing[index]
-id <- dtListing[index,resource_id]
-name <- dtListing[index,resource_name]
+id <- dtListing[index,dataset_id]
+name <- dtListing[index,dataset_name]
 format <- dtListing[index,resource_format]
+
+curl_download(str_c(query,id,"/download"),
+              str_c(dirOutput,name,".zip"))
+
+unzip(str_c(dirOutput,"dataset-listing.zip"),
+      exdir = str_c(dirOutput,"dataset-listing"))
+
+
 
 if(format == "CSV"){
   #downloading dataset
@@ -33,3 +42,7 @@ if(format == "CSV"){
 #unzip(str_c(dirOutput,name,".zip"),
 #      exdir = str_c(dirOutput,name)
 #      )
+dtListing[,!c("description","resource_description")][resource_format=="SHP"]
+
+'https://data.gov.sg/dataset/5589d1bc-d3bd-4d62-923d-7b59b4e822e2/download'
+'https://data.gov.sg/dataset/0d1ace9a-baa2-46ed-b5e3-09b9dcdc60e1/download'
